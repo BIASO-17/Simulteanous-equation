8683dfc5c5487be37079f44f6a8e9ac5669b3ba8
BIASO-17 <ifeoluwa.bankole05@gmail.com>
2023-02-01 22:50:04 +0100

    added styling

---
+++ app.js
@@ -0,0 +1,24 @@
+document.write ( 'a1');
+document.getElementById(a1).innerHTML = a1;
+document.getElementsByName(input)
+/*var a1;
+var b1;
+var c1;
+var a2;
+var b2;
+var c2;
+var x;
+var y;
+a1 = prompt(" "+"    a1=");
+b1 = prompt("b1=");
+c1 = prompt("c1=");
+a2 = prompt("a2=");
+b2 = prompt("b2=");
+c2 = prompt("c2=");
+ 
+ y = ((c2 * a1)-(a2 * c1))/((a1 * b2)-(a2 * b1));
+ x = (1/a1)*(c1 - (b1*y));
+ 
+prompt("x ="+" "+x);
+prompt("y ="+" "+y);
+alert( " x= "+x +" and "+" y= "+ y)
